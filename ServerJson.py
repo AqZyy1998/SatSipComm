6a4737f694b88d918923da474b0079a924299103
AqZyy1998 <aqzyy1998@bupt.edu.cn>
2021-07-05 17:45:51 +0800

    complete json data transfer;

--- ServerJson.py
+++ ServerJson.py
@@ -1,21 +1,26 @@
-import socket,os
-import _thread as thread
-import config
-from config import ServerIp, SatServerIp, ServerJsonPort
+import os
+import socket
 import transFileType
+import base64
+from config import SatServerIp, ServerJsonPort
+
+
+def writeFile(data, address):
+    with open(address, "w") as f:
+        f.write(data)
+    f.close()
 
 
 def ServerJsonRun():
     address = (SatServerIp, ServerJsonPort)  # 卫星服务端地址和端口
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
-        trigger = transFileType.jsonTransferToBinary("Files/serverFile")
+        trigger = transFileType.jsonTransferToBinary("Files/serverFile")  # trigger是二进制流
         s.sendto(trigger.encode(), address)
-        filename, addr = s.recvfrom(1024)  # 返回数据和接入连接的（服务端）地址
-        filename = filename.decode()
-        print('[Recieved]', filename)
-        os.system(r"python " + filename)  # filename带py
-        # startClientSip(filename)
+        satBinaryData, addr = s.recvfrom(1024)  # 返回数据和接入连接的（服务端）地址
+        satStrData = base64.b64decode(satBinaryData).decode()  # string类型的input.py\n output.py
+        print('[Received]', satStrData)
+        writeFile(satStrData, "Files/satJsonData.txt")
         if trigger == '###':  # 自定义结束字符串
             break
     s.close()
--- transFileType.py
+++ transFileType.py
@@ -1,22 +1,27 @@
 import requests
 import base64
+import os
 import json
 
 
 def jsonTransferToBinary(filename):
-    # TODO json文件转换为二进制文件格式
-    with open(filename, "r") as f:
-        fileContent = f.read()
-    encodestr = base64.b64encode(fileContent.encode('utf - 8'))
-    print(encodestr)
-    binaryContent = str(encodestr, 'utf - 8')
-    print(binaryContent)
-    # base64转换成json（解码）
-    ww = base64.b64decode(encodestr)
-    print(str(ww, 'utf - 8'))
-    with open(filename + "InBinary", "w") as f:
-        f.write(binaryContent)
-        f.close()
+    if os.path.getsize(filename) != 0:
+        # TODO json文件转换为二进制文件格式
+        with open(filename, "r") as f:
+            fileContent = f.read()
+        encodestr = base64.b64encode(fileContent.encode('utf - 8'))
+        print(encodestr)
+        binaryContent = str(encodestr, 'utf - 8')
+        print(binaryContent)
+        # base64转换成json（解码）
+        # ww = base64.b64decode(encodestr)
+        # print(str(ww, 'utf - 8'))
+        with open(filename + "InBinary", "w") as f:
+            f.write(binaryContent)
+            f.close()
+        return binaryContent
+    else:
+        return "###"
 
 
 def BinaryTransferToJson(filename):
