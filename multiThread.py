0415a3c6140c5d403b13703b27e1ec62f23bc1ff
AqZyy1998 <aqzyy1998@bupt.edu.cn>
2021-07-06 23:11:39 +0800

    add remote communication

--- multiThread.py
+++ multiThread.py
@@ -1,12 +1,20 @@
 import threading
-import Server, ServerB
+import Server, ServerB, ServerJson, ServerRemote, SatServerJson, SatServerRemote
 from Server import *
 from ServerB import *
 
 if __name__ == '__main__':
     thread1 = threading.Thread(target=Server.ServerRun, args=())
     thread2 = threading.Thread(target=ServerB.ServerBRun, args=())
+    thread3 = threading.Thread(target=ServerJson.ServerJsonRun, args=())
+    thread4 = threading.Thread(target=ServerRemote.ServerRemoteRun, args=())
+    thread5 = threading.Thread(target=SatServerJson.SatServerJsonRun, args=())
+    thread6 = threading.Thread(target=SatServerRemote.SatServerRemoteRun, args=())
     thread1.start()
     thread2.start()
+    thread3.start()
+    thread4.start()
+    thread5.start()
+    thread6.start()
     while 1:
         pass
